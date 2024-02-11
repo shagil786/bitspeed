2e40ef482904935c3016fb2a7add4d542adae9a6
shagil786 <shagil.nizami@fincity.com>
2024-02-11 22:46:52 +0530

    changes

--- App.js
+++ App.js
@@ -106,7 +106,7 @@ app.post("/identify", async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
